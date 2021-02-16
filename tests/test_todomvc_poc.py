3fdd69cf1be7a57acdf5dbc20dd2868fe33d8867
hob83 <buryachek83@gmail.com>
2021-02-16 19:27:56 +0200

    poc2

--- tests/test_todomvc_poc.py
+++ tests/test_todomvc_poc.py
@@ -3,11 +3,12 @@ from selene.support.shared import browser
 import time
 
 
-def test_poc():
+def test_common_management():
 
     browser.config.hold_browser_open = True
     browser.open('https://todomvc4tasj.herokuapp.com/')
-    time.sleep(2)
+    window_uploaded = "return $._data($('#clear-completed')[0], 'events').hasOwnProperty('click')"
+    browser.should(have.js_returned(True, window_uploaded))
 
     browser.element('#new-todo').type('a').press_enter()
     browser.element('#new-todo').type('b').press_enter()
@@ -16,13 +17,12 @@ def test_poc():
     browser.all('#todo-list>li').should(have.exact_texts('a', 'b', 'c', 'd'))
 
     browser.all('#todo-list>li').element_by(have.exact_text('b')).double_click()
-    browser.element('.editing>.edit').type(' edited').press_enter()
+    browser.all('#todo-list>li').element_by(have.css_class('editing')).element('.edit').type(' edited').press_enter()
     browser.all('#todo-list>li').element_by(have.exact_text('a')).element('.toggle').click()
-    browser.all('#todo-list>li').element_by(have.exact_text('b edited')).element('.toggle').click()
     browser.element('#clear-completed').click()
-    browser.all('#todo-list>li').should(have.exact_texts('c', 'd'))
+    browser.all('#todo-list>li').should(have.exact_texts('b edited', 'c', 'd'))
 
     browser.all('#todo-list>li').element_by(have.exact_text('c')).double_click()
-    browser.element('.editing>.edit').type('to be canceled').press_escape()
-    browser.all('#todo-list>li').element_by(have.exact_text('c')).element('.destroy').click()
-    browser.all('#todo-list>li').should(have.exact_texts('d'))
+    browser.all('#todo-list>li').element_by(have.css_class('editing')).element('.edit').type(' edited').press_escape()
+    browser.all('#todo-list>li').element_by(have.exact_text('c')).hover().element('.destroy').click()
+    browser.all('#todo-list>li').should(have.exact_texts('b edited', 'd'))
