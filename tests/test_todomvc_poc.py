cd0e1bb79eddb544dacad81d3d50eb29e29d7469
hob83 <buryachek83@gmail.com>
2021-02-17 21:33:14 +0200

    poc3

--- tests/test_todomvc_poc.py
+++ tests/test_todomvc_poc.py
@@ -1,6 +1,5 @@
 from selene import have
 from selene.support.shared import browser
-import time
 
 
 def test_common_management():
@@ -18,11 +17,11 @@ def test_common_management():
 
     browser.all('#todo-list>li').element_by(have.exact_text('b')).double_click()
     browser.all('#todo-list>li').element_by(have.css_class('editing')).element('.edit').type(' edited').press_enter()
-    browser.all('#todo-list>li').element_by(have.exact_text('a')).element('.toggle').click()
+    browser.all('#todo-list>li').element_by(have.exact_text('b edited')).element('.toggle').click()
     browser.element('#clear-completed').click()
-    browser.all('#todo-list>li').should(have.exact_texts('b edited', 'c', 'd'))
+    browser.all('#todo-list>li').should(have.exact_texts('a', 'c', 'd'))
 
     browser.all('#todo-list>li').element_by(have.exact_text('c')).double_click()
     browser.all('#todo-list>li').element_by(have.css_class('editing')).element('.edit').type(' edited').press_escape()
     browser.all('#todo-list>li').element_by(have.exact_text('c')).hover().element('.destroy').click()
-    browser.all('#todo-list>li').should(have.exact_texts('b edited', 'd'))
+    browser.all('#todo-list>li').should(have.exact_texts('a', 'd'))
